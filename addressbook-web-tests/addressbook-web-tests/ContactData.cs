08ed42111cc32c3ee5b439f06cc63f91372cecc3
mislimova <mislimova@skbkontur.ru>
2025-10-09 01:35:35 +0300

    new test for creating contacts and new classes

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,81 @@
+﻿namespace addressbook_web_tests;
+
+
+public class ContactData
+{
+    private string contactName;
+    private string contactLastName;
+    private string contactPhone;
+    private string contactEmail;
+    private string contactAddress;
+    
+    public ContactData(string contactName, string contactLastName, string contactPhone, string contactEmail, string contactAddress)
+    {
+        this.contactName = contactName;
+        this.contactLastName = contactLastName;
+        this.contactPhone = contactPhone;
+        this.contactEmail = contactEmail;
+        this.contactAddress = contactAddress;
+        
+    }
+    
+    public string Name
+    {
+        get
+        {
+            return contactName;
+        }
+        set
+        {
+            contactName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get
+        {
+            return contactLastName;
+        }
+        set
+        {
+            contactLastName = value;
+        }
+    }
+    
+    public string Phone
+    {
+        get
+        {
+            return contactPhone;
+        }
+        set
+        {
+            contactPhone = value;
+        }
+    }
+    
+    public string Email
+    {
+        get
+        {
+            return contactEmail;
+        }
+        set
+        {
+            contactEmail = value;
+        }
+    }
+    
+    public string Address
+    {
+        get
+        {
+            return contactAddress;
+        }
+        set
+        {
+            contactAddress = value;
+        }
+    }
+}
