fe35afc75d6921f3d09a56bb9d2648d99ead12a9
mislimova <mislimova@skbkontur.ru>
2025-10-25 00:26:34 +0300

    lesson 3.4

--- addressbook-web-tests/addressbook-web-tests/AppManager/NavigationHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/AppManager/NavigationHelper.cs
@@ -16,21 +16,40 @@ public class NavigationHelper : HelperBase
     }
     public void OpenHomePage()
     {
+        if (_driver.Url == _baseURL + "addressbook/group.php")
+        {
+            return;
+        }
         _driver.Navigate().GoToUrl(_baseURL);
     }
     
     public void GoToGroupsPage()
     {
+        if (_driver.Url == _baseURL + "addressbook/group.php"
+            && IsElementPresent(By.Name("new")))
+        {
+            return;
+        }
         _driver.FindElement(By.LinkText("groups")).Click();
     }
     
     public void GoToAddressbookEdit()
     {
+        if (_driver.Url == _baseURL + "addressbook/edit.php"
+            && IsElementPresent(By.CssSelector("#content > form > input[type='file']:nth-child(19)")))
+        {
+            return;
+        }
         _driver.Navigate().GoToUrl("http://localhost/addressbook/edit.php");
     }
 
     public void GoToAddressbookPage()
     {
+        if (_driver.Url == _baseURL + "addressbook/index.php"
+            && IsElementPresent(By.CssSelector("#right > select > option:nth-child(1)")))
+        {
+            return;
+        }
         _driver.Navigate().GoToUrl("http://localhost/addressbook/index.php");
     }
 
