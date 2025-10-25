c1c270c8f7d114c40d35280352e96fa55b13d89b
mislimova <mislimova@skbkontur.ru>
2025-10-26 00:42:54 +0300

    fixing2 les4.1

--- addressbook-web-tests/addressbook-web-tests/Tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/ContactCreationTests.cs
@@ -16,17 +16,18 @@ namespace addressbook_web_tests
         public void UserCanLoginAndCreateContacts()
         {
             // создаём объект ContactData и задаём ему значения
-            ContactData contact = new ContactData("Sara", "Mislimova", "+79614072727", 
-                "mimas19@gmail.com", "Rostov-on-Don");
-
             app.Navigator.GoToAddressbookEdit();
             
             List<ContactData> oldContacts = app.Contact.GetContactList();
             
+            ContactData contact = new ContactData("Sara", "Mislimova", "+79614072727", 
+                "mimas19@gmail.com", "Rostov-on-Don");
+
+            app.Navigator.GoToAddressbookPage();
             app.Contact.CreateContact(contact);
             
             // app.Navigator.GoToAddressbookPage();  попробовать расскомментить если падает тест 
-
+            app.Navigator.GoToAddressbookPage();
             List<ContactData> newContacts = app.Contact.GetContactList(); 
             Assert.AreEqual(oldContacts.Count + 1, newContacts.Count);
             
@@ -36,10 +37,8 @@ namespace addressbook_web_tests
         public void EmptyContactCreationTest()
         {
             // тест на создание пустого контакта
-            ContactData contact = new ContactData("", "", "", "", "");
-            
             app.Navigator.GoToAddressbookEdit();
-            
+            ContactData contact = new ContactData("", "", "", "", "");
             List<ContactData> oldContacts = app.Contact.GetContactList();
             
             app.Contact.CreateContact(contact);
