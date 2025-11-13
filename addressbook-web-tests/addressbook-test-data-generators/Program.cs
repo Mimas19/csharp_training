052869bc991b771c47b20c5073096333600b85d0
mislimova <mislimova@skbkontur.ru>
2025-11-13 23:47:13 +0300

    lesson 6.1 - 1

--- addressbook-web-tests/addressbook-test-data-generators/Program.cs
+++ addressbook-web-tests/addressbook-test-data-generators/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using addressbook_web_tests;
+
 
 namespace addressbook_test_data_generators;
 
@@ -10,6 +13,14 @@ public class Program
 {
     static void Main(string[] args)
     {
-        System.Console.Out.Write("Hello World!");
+        int count = Convert.ToInt32(args[0]);
+        StreamWriter writer = new StreamWriter(args[1]);
+        for (int i = 0; i < count; i++)
+        {
+            writer.WriteLine(String.Format("${0},${1},${2}",
+                TestBase.GenerateRandomString(10),
+                TestBase.GenerateRandomString(100),
+                TestBase.GenerateRandomString(100)));
+        }
     }
 }
