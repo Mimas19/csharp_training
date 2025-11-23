6194dd086df7c89377a03f6360a70ec2855a8832
mislimova <mislimova@skbkontur.ru>
2025-11-23 19:33:59 +0300

    new project for les 8.1

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/TestBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using static NUnit.Framework.Assert;
+
+
+namespace addressbook_tests_autoit
+{
+    public class TestBase
+    {
+      ApplicationManager app;  
+        
+      [OneTimeSetUp]
+      public void initApplication()
+      {
+          app = new ApplicationManager();
+      }
+
+      [OneTimeTearDown]
+      public void stopApplication()
+      {
+          app.Stop();
+      }
+    }
+}
+
