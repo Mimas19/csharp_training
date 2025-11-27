449d2c134f21dd4a242ffb7c0333d38093d8a0e2
mislimova <mislimova@skbkontur.ru>
2025-11-27 21:32:01 +0300

    ex 18 for removal group

--- addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
@@ -53,5 +53,46 @@ namespace addressbook_tests_autoit
             AutoItX.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d512");
             AutoItX.WinWait(GROUPWINTITLE);
         }
+        
+        
+        public void Remove(string groupName)
+        {
+            OpenGroupsDialogue();
+    
+            // Выбираем группу в дереве
+            AutoItX.ControlTreeView(
+                GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51",
+                "Select", "#0|#" + FindGroupIndex(groupName), "");
+    
+            // Нажимаем Delete
+            AutoItX.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d51");
+            
+            //?????????????????????????????????????? вот тут ещё какое-то действие?
+            // меня смущает немного диалоговое окно и как с ним работать с удалением
+    
+            // Подтверждаем удаление (OK)
+            AutoItX.ControlClick("", "", "WindowsForms10.BUTTON.app.0.2c908d53");
+    
+            CloseGroupDialogue();
+        }
+
+        private int FindGroupIndex(string groupName)
+        {
+            string count = AutoItX.ControlTreeView(
+                GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51",
+                "GetItemCount", "#0", "");
+
+            for (int i = 0; i < int.Parse(count); i++)
+            {
+                string item = AutoItX.ControlTreeView(
+                    GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51",
+                    "GetText", "#0|#" + i, "");
+        
+                if (item == groupName)
+                    return i;
+            }
+            return -1;
+        }
+
     }
 }
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/groupRemovalTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using static NUnit.Framework.Assert;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupDeletion()
+        {
+            // Создаю тестовую группу, если её нет
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            GroupData testGroup = new GroupData() { Name = "test" };
+            bool exists = false;
+    
+            foreach (GroupData group in oldGroups)
+            {
+                if (group.Name == "test")
+                {
+                    exists = true;
+                    break;
+                }
+            }
+    
+            if (!exists)
+            {
+                app.Groups.Add(testGroup);
+            }
+
+            // Получаю список до удаления
+            List<GroupData> groupsBefore = app.Groups.GetGroupList();
+
+            // Удаляю
+            app.Groups.Remove(testGroup.Name);
+
+            // Получаю список после удаления и проверяем
+            List<GroupData> groupsAfter = app.Groups.GetGroupList();
+    
+            AreEqual(groupsBefore.Count - 1, groupsAfter.Count);
+            CollectionAssert.DoesNotContain(groupsAfter, testGroup);
+        }
+
+    }
+}
